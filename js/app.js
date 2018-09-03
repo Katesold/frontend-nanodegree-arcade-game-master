6fe2e52ab53bfbe3f33a08dbd03804cb1cdc7286
Kata Katona <katalap@gmail.com>
2018-09-03 21:33:34 +0100

    cleaned up comments

--- js/app.js
+++ js/app.js
@@ -4,13 +4,12 @@ let wins = 0;
 const won = document.querySelector('div');
 const head = document.querySelector('h1');
 
-// Enemies our player must avoid
+// Enemies the player must avoid
 class Enemy {
     // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
 
     // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
+    // a helper to load images
     constructor(x, y, speed) {
         this.sprite = 'images/enemy-bug.png';
         this.x = x;
@@ -23,7 +22,7 @@ class Enemy {
     // Update the enemy's position, required method for game
     // Parameter: dt, a time delta between ticks
     update(dt) {
-        // You should multiply any movement by the dt parameter
+        // multiply any movement by the dt parameter
         // which will ensure the game runs at the same speed for
         // all computers.
         this.x += this.speed * dt;
@@ -41,8 +40,7 @@ class Enemy {
 
 
 
-// Now write your own player class
-// This class requires an update(), render() and
+// player class requires an update(), render() and
 // a handleInput() method.
 class Player {
     constructor(x, y) {
@@ -100,8 +98,7 @@ class Player {
 }
 
 
-// Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
+//instantiate objects. All enemy objects are in an array
 const allEnemies = [];
 const enemy1 = new Enemy(-60, 60, Math.floor(Math.random() * 200) + 100);
 const enemy2 = new Enemy(60, 60, Math.floor(Math.random() * 200) + 100);
@@ -128,8 +125,7 @@ const player = new Player(200, 390);
     para.innerHTML = `<p>Wins: </p> + wins`;
 }*/
 
-// This listens for key presses and sends the keys to your
-// Player.handleInput() method. You don't need to modify this.
+// This listens for key presses and sends the keys to Player.handleInput() method. 
 document.addEventListener('keyup', function(e) {
     var allowedKeys = {
         37: 'left',
